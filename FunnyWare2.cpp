3390a74bff189c07f043ceb8847ac4785979d7fd
Duhamster <ergerg>
2025-12-27 09:12:38 +0300

    Added GetModuleHandle2()

--- FunnyWare2.cpp
+++ FunnyWare2.cpp
@@ -2,6 +2,72 @@
 #include <windows.h>
 #include <winternl.h>
 #include <string.h>
+#include <stdlib.h>
+
+bool MatchDllName(const UNICODE_STRING* fullDllName, LPCSTR lpModuleName) {
+    if (fullDllName->Length == 0 || lpModuleName[0] == '\0') return false;
+    char moduleUpper[256] = { 0 };
+    size_t len = strlen(lpModuleName);
+    for (size_t i = 0; i < len && i < 255; i++) {
+        moduleUpper[i] = (char)toupper(lpModuleName[i]);
+    }
+
+    size_t asciiLen = strlen(moduleUpper);
+    PWCHAR wptr = fullDllName->Buffer;
+    size_t wlen = fullDllName->Length / sizeof(WCHAR);
+    size_t matchLen = 0;
+    size_t wstart = wlen;
+
+    while (wstart > 0 && matchLen < asciiLen) {
+        wstart--;
+        if (towupper(wptr[wstart]) == (WCHAR)moduleUpper[asciiLen - 1 - matchLen]) {
+            matchLen++;
+        }
+        else {
+            matchLen = 0;
+        }
+    }
+
+    return (matchLen == asciiLen);
+}
+
+
+inline PPEB GetCurrentPEB() {
+#ifdef _M_X64
+    return (PPEB)__readgsqword(0x60); // Read 64-bit value at GS:0x60
+#elif _M_IX86
+    return (PPEB)__readfsdword(0x30);
+#endif
+}
+
+void printAllDlls() {
+    PPEB peb = GetCurrentPEB();
+    PPEB_LDR_DATA ldr = peb->Ldr;
+    PLIST_ENTRY head = &(ldr->InMemoryOrderModuleList);
+    PLIST_ENTRY curr = head->Flink;
+    while (curr != head) {
+        PLDR_DATA_TABLE_ENTRY entry = reinterpret_cast<PLDR_DATA_TABLE_ENTRY>(reinterpret_cast<BYTE*>(curr) - 16);
+        UNICODE_STRING dll_name = entry->FullDllName;
+        std::wcout << dll_name.Buffer << std::endl;
+        curr = curr->Flink;
+    }
+}
+
+HMODULE GetModuleHandle2(LPCSTR lpModuleName) {
+    PPEB peb = GetCurrentPEB();
+    PPEB_LDR_DATA ldr = peb->Ldr;
+    PLIST_ENTRY head = &(ldr->InMemoryOrderModuleList);
+    PLIST_ENTRY curr = head->Flink;
+    while (curr != head) {
+        PLDR_DATA_TABLE_ENTRY entry = reinterpret_cast<PLDR_DATA_TABLE_ENTRY>(reinterpret_cast<BYTE*>(curr) - 16);
+        UNICODE_STRING dll_name = entry->FullDllName;
+        if (MatchDllName(&dll_name, lpModuleName)) {
+            return reinterpret_cast<HMODULE>(entry->DllBase);
+        }
+        curr = curr->Flink;
+    }
+    return 0;
+}
 
 void* GetProcAddress2(HMODULE hModule, LPCSTR lpProcName) {
     IMAGE_DOS_HEADER* library_msdos = reinterpret_cast<IMAGE_DOS_HEADER*>(hModule);
@@ -31,7 +97,7 @@ void* GetProcAddress2(HMODULE hModule, LPCSTR lpProcName) {
 
 int main()
 {
-    HMODULE k32 = GetModuleHandleA("kernel32.dll");
-    std::cout << GetProcAddress2(k32, "GetProcAddress") << std::endl;
-    std::cout << GetProcAddress(k32, "GetProcAddress") << std::endl;
+    HMODULE k32 = GetModuleHandle2("kernel32.dll");
+    void* LoadLibraryA = GetProcAddress2(k32, "LoadLibraryA");
+    void* VirtualAlloc = GetProcAddress2(GetModuleHandle2("kernel32.dll"), "VirtualAlloc");
 }
